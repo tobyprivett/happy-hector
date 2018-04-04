b8452aa25851ac92416e62300b108e648705821c
Toby Privett <toby@snaplab.co.uk>
2018-04-04 18:41:56 +0100

    Rubocop fixes

--- app/services/freeagent_client.rb
+++ app/services/freeagent_client.rb
@@ -7,12 +7,12 @@ class FreeagentClient
     self.class.headers 'Accept': 'application/json'
     self.class.headers 'Content-Type': 'application/json'
     self.class.headers 'Authorization': "Bearer #{auth_user.current_auth_token}"
-    self.class.headers  'User-Agent': 'Httparty'
+    self.class.headers 'User-Agent': 'Httparty'
   end
 
   def profit_and_loss_summary
     self.class.get(
-      "/v2/accounting/profit_and_loss/summary"
+      '/v2/accounting/profit_and_loss/summary'
     ).parsed_response['profit_and_loss_summary']
   end
 end
