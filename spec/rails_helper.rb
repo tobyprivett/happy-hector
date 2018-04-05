8bf82550a114f4c56a7cc114409b76d64c95bd96
Toby Privett <toby@snaplab.co.uk>
2018-04-05 10:19:46 +0100

    Prefer single quotes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,7 +9,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 
-require "rack_session_access/capybara"
+require 'rack_session_access/capybara'
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
